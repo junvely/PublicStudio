a319f5f34229ccaff266e6920e01743a826c630b
junvely <junvely97@gmail.com>
2023-05-03 04:10:36 +0900

    Fix : NewPostList에서 data값을 가져오기 전 undefined일 시 발생하는 에러 핸들링

--- src/redux/components/NewPostsList.jsx
+++ src/redux/components/NewPostsList.jsx
@@ -6,9 +6,14 @@ import { StNewPostList, StNewPostsCon } from "styles/Components";
 import { StFlexCon } from "styles/GlobalStyles";
 
 function NewPostsList({ isActive }) {
-  const { data } = useQuery("posts", getPostsAxios);
-  const postList = data.slice(0, 4);
-  // Posts최신순
+  const { isLoading, isError, data } = useQuery("posts", getPostsAxios);
+  // data가 할당되기 전 undefined일 경우 에러가 발생하지 않도록 핸들링
+  if (!data) {
+    return;
+  }
+
+  const postList = [...data].slice(0, 4);
+  // Posts최신순으로 변경하고 싶을 경우
   const newList = [...postList].reverse().slice(0, 4);
 
   return (
@@ -16,7 +21,7 @@ function NewPostsList({ isActive }) {
       {/* News active? */}
       {isActive ? (
         <StFlexCon>
-          {postList.map((post) => {
+          {postList?.map((post) => {
             return (
               <StNewPostList key={post.id}>
                 <Link to={`/posts/${post.id}`}>'{post.title}'</Link>
